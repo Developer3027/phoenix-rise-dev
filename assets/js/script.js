ee8eafb66ad1e330d33cc4b2674822e0458c5d21
Mason Roberts <mrobe18@gmail.com>
2023-07-27 13:12:39 -0400

    changed yellow color, work with swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,24 @@
  * Testimonial  Functionality
  */
 
-const btn = document.getElementsByClassName("li-btn");
-const slide = document.getElementById("slide");
+// const btn = document.getElementsByClassName("li-btn");
+// const slide = document.getElementById("slide");
 
-btn[0].addEventListener('click', function (){
-  slide.style.transform = "translateX(0px)";
-  for(let i=0; i < 2; i++) {
-    btn[i].classList.remove("li-active");
-  }
-  this.classList.add("li-active");
-})
+// btn[0].addEventListener('click', function (){
+//   slide.style.transform = "translateX(0px)";
+//   for(let i=0; i < 2; i++) {
+//     btn[i].classList.remove("li-active");
+//   }
+//   this.classList.add("li-active");
+// })
 
-btn[1].addEventListener("click", function () {
-  slide.style.transform = "translateX(-800px)";
-  for (let i = 0; i < 2; i++) {
-    btn[i].classList.remove("li-active");
-  }
-  this.classList.add("li-active");
-});
+// btn[1].addEventListener("click", function () {
+//   slide.style.transform = "translateX(-800px)";
+//   for (let i = 0; i < 2; i++) {
+//     btn[i].classList.remove("li-active");
+//   }
+//   this.classList.add("li-active");
+// });
 
 /**
  * navbar toggle
@@ -66,5 +66,27 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
   window.scrollY >= 50 ? header.classList.add("active")
     : header.classList.remove("active");
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
 
